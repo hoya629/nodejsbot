0c53e201a0314e5f0e689d5e0d5cb403df1e1be3
hoya629 <60085005+hoya629@users.noreply.github.com>
2020-06-20 19:03:37 +0900

    add clear command

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content == 'ping') {
-    return message.reply('pong');
+  if(message.content == '야') {
+    return message.reply('반말? 지건 딱대');
   }
 
   if(message.content == 'embed') {
